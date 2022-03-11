b2476568a1ec6e3729c2cc8b0cf82ab520b0ef3c
Junwoo Park <JunwooDev@gmail.com>
2022-03-11 09:33:44 -0500

    update Navbar with react.js style #28

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -22,13 +22,19 @@ const Navbar = (props) => {
     // }, [isActive])
 
     return (
-        <nav className={isActive ? "nav-bar active" : "nav-bar"}>
-            <button onClick={() => {setActive(prevState => !prevState)}}>Test</button>
-            <NavLink to={ROUTE_LOGIN}>Login</NavLink>
-            <NavLink to={ROUTE_DASHBOARD} >Dashboard</NavLink>
-            <NavLink to={ROUTE_BUILDER}>Resume Builder</NavLink>
-            <NavLink to={ROUTE_SETTINGS}>Settings</NavLink>
-        </nav>
+        <React.Fragment>
+            <div className="nav-bar padding"/>
+            <nav className={isActive ? "nav-bar active" : "nav-bar"}>
+                <button onClick={() => {
+                    setActive(prevState => !prevState)
+                }}>Test
+                </button>
+                <NavLink to={ROUTE_LOGIN}>Login</NavLink>
+                <NavLink to={ROUTE_DASHBOARD}>Dashboard</NavLink>
+                <NavLink to={ROUTE_BUILDER}>Resume Builder</NavLink>
+                <NavLink to={ROUTE_SETTINGS}>Settings</NavLink>
+            </nav>
+        </React.Fragment>
     )
 }
 
