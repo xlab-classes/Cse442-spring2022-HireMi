1b5b2e2c7d99de8a584646a72e933ae5a2f7dda1
iquitgame <admm64@gmail.com>
2022-03-18 23:10:17 -0400

    Asif's logic for settings #43 #44 #45

--- frontend/src/components/Settings/Settings.js
+++ frontend/src/components/Settings/Settings.js
@@ -1,53 +1,119 @@
 import React, {useState, useEffect} from 'react';
 import Cookies from "js-cookie";
 
-import styles from "../Settings/Settings.module.scss"
+import styles from "../Settings/Settings.module.scss";
 
 const Settings = ({auth, setAuth}) => {
 
-    const [isEdit, setEdit] = useState(false)
-    const [username, setName] = useState(auth?.username)
-    const [pic, setPic] = useState(auth?.pic)
+    const [isEdit, setEdit] = useState(false);
+    const [username, setName] = useState(auth?.username);
+    const [pic, setPic] = useState((auth?.pic.length > 1000) ? URL.createObjectURL(atob(auth?.pic)) : "https://lh3.googleusercontent.com/a/AATXAJxOjQQoJshWIHJ0t67X0-fqBJzgTDMnMcCaHvqy=s96-c");
     const [file, setFile] = useState(null);
 
     useEffect(() => {
         if(!file) {
-            setPic(auth?.pic)
+            setPic((auth?.pic.length > 1000) ? URL.createObjectURL(atob(auth?.pic)) : "https://lh3.googleusercontent.com/a/AATXAJxOjQQoJshWIHJ0t67X0-fqBJzgTDMnMcCaHvqy=s96-c");
             return;
         }
-        const objectUrl = URL.createObjectURL(file)
-        setPic(objectUrl)
+        const objectUrl = URL.createObjectURL(file);
+        setPic(objectUrl);
         // setFile(selectedFile)
 
         // free memory when ever this component is unmounted
-        return () => URL.revokeObjectURL(objectUrl)
-    }, [file])
+        return () => URL.revokeObjectURL(objectUrl);
+    }, [file]);
 
     function onSelect(e) {
         if (!e.target.files || e.target.files.length === 0) {
-            setFile(null)
-            return
+            setFile(null);
+            return;
         }
-        setFile(e.target.files[0])
+        setFile(e.target.files[0]);
     }
 
     function discardPic() {
-        setPic(auth?.pic)
-        setFile(null)
+        setPic((auth?.pic.length > 1000) ? URL.createObjectURL(atob(auth?.pic)) : "https://lh3.googleusercontent.com/a/AATXAJxOjQQoJshWIHJ0t67X0-fqBJzgTDMnMcCaHvqy=s96-c");
+        setFile(null);
+    }
+
+    function uploadImage() {
+        // Implement uploading to database through backend api with token
+
+        const encoded_image = btoa(file);
+        const json_body = {'id': auth?.id, 'image': encoded_image};
+        const string_body = JSON.stringify(json_body);
+
+        fetch('https://www-student.cse.buffalo.edu/CSE442-542/2022-Spring/cse-442r/backend/api/api.php/profile_pic', {
+            method: 'POST',
+            // mode: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + auth?.access_token
+            },
+            body: string_body
+        }).then((result) => console.log(result));
+
+        setAuth({
+            username: auth?.username,
+            id: auth?.id,
+            pic: encoded_image, // Change the profile picture
+            email: auth?.email,
+            access_token: auth?.access_token,
+        })
     }
 
     function discardName() {
-        setName(auth?.username)
+        setName(auth?.username);
+    }
+
+    function changeName() {
+
+        const json_body = {'id': auth?.id, 'new_name': username};
+        const string_body = JSON.stringify(json_body);
+
+        // Implement changing name in database through backend api with token
+        fetch('https://www-student.cse.buffalo.edu/CSE442-542/2022-Spring/cse-442r/backend/api/api.php/change_name', {
+            method: 'POST',
+            // mode: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + auth?.access_token
+            },
+            body: string_body
+        }).then((result) => console.log(result));
+
+        setAuth({
+            username: username, // Change the username
+            id: auth?.id,
+            pic: auth?.pic,
+            email: auth?.email,
+            access_token: auth?.access_token,
+        })
     }
 
     function deleteAccount() {
-        // Add API that does it
+        // Implement profile deletion in database through backend api with token
+        const json_body = {'id': auth?.id};
+        const string_body = JSON.stringify(json_body);
 
-        Cookies.remove('name')
-        Cookies.remove('id')
-        Cookies.remove('pic')
-        Cookies.remove('email')
-        Cookies.remove('token')
+        fetch('https://www-student.cse.buffalo.edu/CSE442-542/2022-Spring/cse-442r/backend/api/api.php/del_acc', {
+            method: 'POST',
+            // mode: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + auth?.access_token
+            },
+            body: string_body
+        }).then((result) => console.log(result));
+
+        Cookies.remove('name');
+        Cookies.remove('id');
+        Cookies.remove('pic');
+        Cookies.remove('email');
+        Cookies.remove('token');
 
         setAuth({
             username: "Anonymous",
@@ -66,13 +132,13 @@ const Settings = ({auth, setAuth}) => {
                     <span>Change Photo</span>
                     <img alt="profile photo" src={pic}/>
                     <input type='file' accept="image/*" onChange={onSelect}/>
-                    <button onClick={null} disabled={pic === auth?.pic}>Update</button>
+                    <button onClick={uploadImage} disabled={pic === auth?.pic}>Update</button>
                     {file ? <button onClick={discardPic}>Discard Changes</button> : null}
                 </div>
                 <div>
                     <span>Change Name</span>
                     <input type='text' defaultValue={username} value={username} onChange={e => setName(e.target.value)}/>
-                    <button onClick={null} disabled={username === auth?.username}>Update</button>
+                    <button onClick={changeName} disabled={username === auth?.username}>Update</button>
                     {username === auth?.username ? null : <button onClick={discardName}>Discard Changes</button>}
                 </div>
                 <div>
