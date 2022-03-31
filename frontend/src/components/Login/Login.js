96d80013e4d576167b47d78307f1651a9a627875
Junwoo Park <JunwooDev@gmail.com>
2022-03-31 01:29:06 -0400

    add error prompt in the login page as part of design #56

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {GoogleLogin} from "react-google-login"
@@ -9,20 +9,22 @@ import g_logo from './img/g_logo.svg';
 
 
 function Login({setAuth, auth}) {
+    const [isError, setError] = useState(false);
+    const [errMsg, setErrMsg] = useState('');
+
     return (
         <div className={styles['page-root']}>
             <div className={styles['login-wrap']}>
                 <img className={styles['']} src={hiremi_logo} alt="hiremi logo"/>
-                <h1 className='row2'>
-                    Welcome Back!
-                </h1>
-                <LoginButton className='row4' setAuth={setAuth} auth={auth}/>
+                <h1 className='row2'>Welcome Back!</h1>
+                <LoginButton className='row4' setAuth={setAuth} auth={auth} setError={setError} setErrMsg={setErrMsg}/>
+                {isError ? <p>Failed to login: {errMsg}</p> : null}
             </div>
         </div>
     );
 }
 
-function LoginButton({setAuth, auth}) {
+function LoginButton({setAuth, auth, setError, setErrMsg}) {
 
     const clientId = "515535950425-qkoa6f2on1o99chdj02nqqqu5e4kln8c.apps.googleusercontent.com";
 
@@ -40,37 +42,42 @@ function LoginButton({setAuth, auth}) {
 
         // console.log(response);
         let verified_response =
-        fetch('./backend/api/api.php/login', {
-            method: 'POST',
-            // mode: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: string_body
-        }).then((result) => result.json())
-            .then((resultJson) => {
+            fetch('./backend/api/api.php/login', {
+                method: 'POST',
+                // mode: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: string_body
+            })
+                .then((result) => result.json())
+                .then((resultJson) => {
+                    const expirationDate = new Date(new Date().getTime() + (14 * 24 * 60 * 60 * 1000));
 
-                const expirationDate = new Date(new Date().getTime() + (14 * 24 * 60 * 60 * 1000));
-
-                document.cookie = `name=${resultJson.name}; path=/; expires=${expirationDate}`;
-                document.cookie = `id=${resultJson.id}; path=/; expires=${expirationDate}`;
-                document.cookie = `pic=${resultJson.pic}; path=/; expires=${expirationDate}`;
-                document.cookie = `email=${resultJson.email}; path=/; expires=${expirationDate}`;
-                document.cookie = `token=${resultJson.verified_token}; path=/; expires=${expirationDate}`;
-                setAuth({
-                    ...auth,
-                    username: resultJson.name,
-                    id: resultJson.id,
-                    pic: resultJson.pic,
-                    email: resultJson.email,
-                    access_token: resultJson.verified_token
+                    document.cookie = `name=${resultJson.name}; path=/; expires=${expirationDate}`;
+                    document.cookie = `id=${resultJson.id}; path=/; expires=${expirationDate}`;
+                    document.cookie = `pic=${resultJson.pic}; path=/; expires=${expirationDate}`;
+                    document.cookie = `email=${resultJson.email}; path=/; expires=${expirationDate}`;
+                    document.cookie = `token=${resultJson.verified_token}; path=/; expires=${expirationDate}`;
+                    setAuth({
+                        ...auth,
+                        username: resultJson.name,
+                        id: resultJson.id,
+                        pic: resultJson.pic,
+                        email: resultJson.email,
+                        access_token: resultJson.verified_token
+                    })
                 })
-            });
+                .catch((err) => {
+                    handleFailure(err);
+                })
 
     }; //end of handleCredentialResponse
 
     const handleFailure = err => {
+        setError(true);
+        setErrMsg(JSON.stringify(err));
         console.error(err)
     }
 
