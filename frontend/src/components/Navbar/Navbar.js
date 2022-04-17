6d0b19e0407303913afbd93d3052ff28c7fba480
Asif Iftekhar <52173133+AsifIftekhar2@users.noreply.github.com>
2022-04-17 17:35:06 -0400

    Fixed GET request and image display bugs in settings page and navbar #80

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 import {NavLink, useLocation} from "react-router-dom";
 import {ROUTE_BUILDER, ROUTE_DASHBOARD, ROUTE_LOGIN, ROUTE_SETTINGS} from "../../constants/routes";
@@ -8,9 +8,79 @@ import styles from './Navbar.module.scss'
 
 
 const Navbar = ({auth}) => {
-    const [isActive, setActive] = useState(false)
+    const [isActive, setActive] = useState(false);
+    const [username, setName] = useState(auth?.username);
+    const [pic, setPic] = useState(auth?.pic);
+    const [nameCounter, setNameCounter] = useState(0);
+    const [picCounter, setPicCounter] = useState(0);
 
     const {pathname} = useLocation();
+
+    useEffect(() => {
+        const json_body = {'id': auth?.id};
+        const string_body = JSON.stringify(json_body);
+
+        fetch('https://www-student.cse.buffalo.edu/CSE442-542/2022-Spring/cse-442r/backend/api/api.php/get_profile_info', {
+            method: 'POST',
+            // mode: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + auth?.access_token
+            },
+            body: string_body
+        }).then((result) => result.json())
+        .then((resultJson) => {
+            console.log(resultJson);
+
+            setName(resultJson.profile_name);
+
+            const count = nameCounter;
+            setNameCounter(count + 1);
+        });
+    }, [auth?.username]);
+
+    useEffect(() => {
+        const json_body = {'id': auth?.id};
+        const string_body = JSON.stringify(json_body);
+
+        fetch('https://www-student.cse.buffalo.edu/CSE442-542/2022-Spring/cse-442r/backend/api/api.php/get_profile_info', {
+            method: 'POST',
+            // mode: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + auth?.access_token
+            },
+            body: string_body
+        }).then((result) => result.json())
+        .then((resultJson) => {
+            console.log(resultJson);
+
+            setPic(resultJson.profile_picture);
+
+            const count = picCounter;
+            setPicCounter(count + 1);
+        });
+    }, [auth?.pic]);
+
+    function b64toBlob(b64Data, contentType='', sliceSize=512) {
+        const byteCharacters = atob(b64Data);
+        const byteArrays = [];
+      
+        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+          const slice = byteCharacters.slice(offset, offset + sliceSize);
+      
+          const byteNumbers = new Array(slice.length);
+          for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+          }
+      
+          const byteArray = new Uint8Array(byteNumbers);
+          byteArrays.push(byteArray);
+        }
+      
+        const blob = new Blob(byteArrays, {type: contentType});
+        return blob;
+      }
 
     return (
         <>
@@ -19,9 +89,9 @@ const Navbar = ({auth}) => {
                     <div className={styles['nav-contents']}>
                         <div className={styles['user-info']}>
                             <NavLink className={styles['nav-link']} to={ROUTE_SETTINGS}>
-                                <img src={auth?.pic}/>
+                                <img src={(picCounter > 1) ? ((auth?.pic.length > 1000) ? URL.createObjectURL(b64toBlob(auth?.pic)) : auth?.pic) : ((pic.length > 1000) ? URL.createObjectURL(b64toBlob(pic)) : pic)}/>
                             </NavLink>
-                            <span>{auth?.username}</span>
+                            <span>{(nameCounter > 1) ? auth?.username : username}</span>
                         </div>
                         <button className={styles['toggle-btn']} onClick={() => setActive(prevState => !prevState)}>
                             <span />
