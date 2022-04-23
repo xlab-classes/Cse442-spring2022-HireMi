ffc41b66cc55f6eb7b2c0b1e333828027407551c
Junwoo Park <JunwooDev@gmail.com>
2022-04-23 17:34:39 -0400

    fix bugs on offset changes #88

--- frontend/src/components/Builder/Builder.js
+++ frontend/src/components/Builder/Builder.js
@@ -230,8 +230,8 @@ const Builder = ({auth, resume, setEditor, setResume}) => {
                 "content": "text element",
                 "offset-x": 0,
                 "offset-y": 0,
-                "width": 'auto',
-                "height": 'auto',
+                // "width": 'auto',
+                // "height": 'auto',
                 "z-index": 1,
                 "prop": {"font-type": "arial", "font-size": 12}
             },
