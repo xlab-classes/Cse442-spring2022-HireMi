b2d402173d29ddcc98242894e1643e143f4c3d81
jing-ao <jingaosu@buffalo.edu>
2022-05-07 14:02:45 -0400

    not found page #94

--- frontend/src/components/NotFound/NotFound.js
+++ frontend/src/components/NotFound/NotFound.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import {ROUTE_DASHBOARD} from "../../constants/routes";
 import styles from './NotFound.module.scss';
-
+import {NavLink} from "react-router-dom";
 const NotFound = props => {
 
     return (
         <div className={styles['page-root']}>
-            <div style={{color: "red", fontSize: "10rem"}}>NotFound</div>
+            <h1>OOPS!</h1>
+            <h2>We can't seem to find the page you are looking for</h2>
+            <p>error code: 404</p>
+            <NavLink to={ROUTE_DASHBOARD}>Back to dashaboard</NavLink>
         </div>
 
     )
